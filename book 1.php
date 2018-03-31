8c705086801fb5e29c09ffcf6d16a984f6b03afa
sasmitha chandana <35595963+sasmithachandana@users.noreply.github.com>
2018-03-31 15:32:10 +0530

    Add files via upload

---
+++ book 1.php
@@ -0,0 +1,60 @@
+<?php
+ require 'dbconfig/config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>*******</title>
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	 <form class="myform" action="book 1.php" method="post">
+	   
+	   <label><b>First Name</label>
+	   <input name="firstname" type="text" class="inputvalues" placeholder="Type your First Name " required/><br>
+	   
+	   <label><b>Last Name</label>
+	   <input name="lastname" type="text" class="inputvalues" placeholder="Type your Last Name" required/><br>
+	   
+         <label><b>National Id Number</label>
+	   <input name="nic" type="text" class="inputvalues" placeholder="Type your National Id Number" required/><br>
+	 
+       <label><b>Email</label>
+	   <input name="email" type="text" class="inputvalues" placeholder="Type your Email" required/><br>
+	 
+       <label><b>Phone Number</label>
+	   <input name="phone" type="text" class="inputvalues" placeholder="Type your Phone Number" required/><br><br>
+	 	 
+
+	   <input name="login" type="submit" id="login_btn" value="Login"/>
+	 
+	 </form>
+	 
+	 <?php
+	   if(isset($_POST['login']))
+	   {
+			$firstname=$_POST['firstname'];
+			$lastname=$_POST['lastname'];
+			$nic=$_POST['nic'];
+			$email=$_POST['email'];
+			$phone=$_POST['phone'];
+			
+			$query2="select * from customer WHERE seatno='1'";
+			$query_run=mysqli_query($con,$query2);
+			
+              if(mysqli_num_rows($query_run)>0){
+			  echo '<script type="text/javascript">alert("Seat Already Booked")</script>';
+			  }
+              else
+			  {
+			  $query="INSERT INTO customer(seatno,firstname,lastname,nic,email,phone) VALUES ('1','$firstname','$lastname','$nic','$email','$phone')";
+			  mysqli_query($con,$query);	 
+			 } 
+
+	   }		
+	 ?>
+</body> 	 
+</html>
---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+.bg {
+    /* The image used */
+    background-image: url("img_girl.jpg");
+
+    /* Full height */
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+</head>
+
+<img src="1.png" usemap="#image-map">
+<map name="image-map">
+    <area  href="book 1.php" coords="398,557,468,500" shape="rect">
+    <area  href="book 2.php" coords="413,502,479,456" shape="rect">
+	<area  href="book 3.php" coords="477,456,420,402" shape="rect">
+	<area  href="book 4.php" coords="486,415,429,361" shape="rect">
+	<area  href="book 5.php" coords="497,368,440,314" shape="rect">
+	<area  href="book 6.php" coords="462,226,504,275" shape="rect">
+	<area  href="book 7.php" coords="465,189,511,225" shape="rect">
+	<area  href="book 8.php" coords="472,166,517,193" shape="rect">
+	<area  href="book 9.php" coords="478,137,523,164" shape="rect">
+	<area  href="book 10.php" coords="480,108,525,135" shape="rect">
+</map>
+</div>
+</html>
+
